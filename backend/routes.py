70a08e12d2d1c49ca5ef1675a35a2150fcdc9b7f
RoyH11 <ruoqiuhuang@gmail.com>
2025-03-20 12:52:27 -0500

    get all users api

--- backend/routes.py
+++ backend/routes.py
@@ -1,10 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from models import db, User, Task
 
 # create a blueprint for routes
 routes = Blueprint('routes', __name__)
 
-
+# Home
 @routes.route('/')
 def home(): 
     return {"mesasge": "Flask API is running!"}
+
+# Get all users
+@routes.route('/users', methods=['GET'])
+def get_users(): 
+    users = User.query.all()
+    user_list = []
+    for u in users: 
+        user_list.append({
+            "id": u.id,
+            "username": u.username
+        })
+    return jsonify(user_list)
 
